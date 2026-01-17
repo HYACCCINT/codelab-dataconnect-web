dd09717721a26560c756f0571bd3ae534ce80387
Cynthia Wang <hyacccint@gmail.com>
2026-01-16 21:27:03 -0500

    fix: add dev nix

---
+++ .idx/dev.nix
@@ -0,0 +1,34 @@
+{pkgs}: {
+  channel = "stable-24.11";
+  packages = [
+    pkgs.nodejs_20
+    pkgs.npm
+    pkgs.jdk17
+    pkgs.zip
+  ];
+  idx.extensions = [
+    "GoogleCloudTools.firebase-dataconnect-vscode"
+    "GraphQL.vscode-graphql-syntax"
+  ];
+  idx.workspace.onCreate = {
+    install-dependencies = "cd app && npm install";
+  };
+  idx.workspace.onStart = {
+    start-emulators = "npx -y firebase-tools@latest emulators:start";
+  };
+  idx.previews = {
+    enable = true; 
+    previews = {
+      web = {
+        command = [
+          "npm"
+          "run" 
+          "dev:emulator" 
+          "--port"
+          "$PORT" 
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
